54712b544ae5e34cfb905d6b865e0ff98795a1dc
acoshift <acoshift@gmail.com>
2019-07-18 19:22:07 +0700

    metric: add namespace and ingress to backend conns

--- metric/backendconns.go
+++ metric/backendconns.go
@@ -22,25 +22,27 @@ func init() {
 	_backendConnections.connections = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: prom.Namespace,
 		Name:      "backend_connections",
-	}, []string{"backend", "service_type", "service_name"})
+	}, []string{"backend", "service_namespace", "service_type", "service_name", "ingress"})
 	_backendConnections.reads = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: prom.Namespace,
 		Name:      "backend_network_read_bytes",
-	}, []string{"backend", "service_type", "service_name"})
+	}, []string{"backend", "service_namespace", "service_type", "service_name", "ingress"})
 	_backendConnections.writes = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: prom.Namespace,
 		Name:      "backend_network_write_bytes",
-	}, []string{"backend", "service_type", "service_name"})
+	}, []string{"backend", "service_namespace", "service_type", "service_name", "ingress"})
 	prom.Registry().MustRegister(_backendConnections.connections)
 	prom.Registry().MustRegister(_backendConnections.reads)
 	prom.Registry().MustRegister(_backendConnections.writes)
 }
 
-func (p *backendConnections) inc(addr string, serviceType string, serviceName string) {
+func (p *backendConnections) inc(addr string, namespace, serviceType, serviceName, ingress string) {
 	c, err := p.connections.GetMetricWith(prometheus.Labels{
-		"backend":      addr,
-		"service_type": serviceType,
-		"service_name": serviceName,
+		"backend":           addr,
+		"service_namespace": namespace,
+		"service_type":      serviceType,
+		"service_name":      serviceName,
+		"ingress":           ingress,
 	})
 	if err != nil {
 		return
@@ -48,11 +50,13 @@ func (p *backendConnections) inc(addr string, serviceType string, serviceName st
 	c.Inc()
 }
 
-func (p *backendConnections) dec(addr string, serviceType string, serviceName string) {
+func (p *backendConnections) dec(addr string, namespace, serviceType, serviceName, ingress string) {
 	c, err := p.connections.GetMetricWith(prometheus.Labels{
-		"backend":      addr,
-		"service_type": serviceType,
-		"service_name": serviceName,
+		"backend":           addr,
+		"service_namespace": namespace,
+		"service_type":      serviceType,
+		"service_name":      serviceName,
+		"ingress":           ingress,
 	})
 	if err != nil {
 		return
@@ -60,11 +64,13 @@ func (p *backendConnections) dec(addr string, serviceType string, serviceName st
 	c.Dec()
 }
 
-func (p *backendConnections) read(addr string, serviceType string, serviceName string, n int) {
+func (p *backendConnections) read(addr string, namespace, serviceType, serviceName, ingress string, n int) {
 	c, err := p.reads.GetMetricWith(prometheus.Labels{
-		"backend":      addr,
-		"service_type": serviceType,
-		"service_name": serviceName,
+		"backend":           addr,
+		"service_namespace": namespace,
+		"service_type":      serviceType,
+		"service_name":      serviceName,
+		"ingress":           ingress,
 	})
 	if err != nil {
 		return
@@ -72,11 +78,13 @@ func (p *backendConnections) read(addr string, serviceType string, serviceName s
 	c.Add(float64(n))
 }
 
-func (p *backendConnections) write(addr string, serviceType string, serviceName string, n int) {
+func (p *backendConnections) write(addr string, namespace, serviceType, serviceName, ingress string, n int) {
 	c, err := p.writes.GetMetricWith(prometheus.Labels{
-		"backend":      addr,
-		"service_type": serviceType,
-		"service_name": serviceName,
+		"backend":           addr,
+		"service_namespace": namespace,
+		"service_type":      serviceType,
+		"service_name":      serviceName,
+		"ingress":           ingress,
 	})
 	if err != nil {
 		return
@@ -88,14 +96,18 @@ func (p *backendConnections) write(addr string, serviceType string, serviceName 
 func BackendConnections(ctx context.Context, conn net.Conn, addr string) net.Conn {
 	serviceType, _ := logger.Get(ctx, "serviceType").(string)
 	serviceName, _ := logger.Get(ctx, "serviceName").(string)
+	namespace, _ := logger.Get(ctx, "namespace").(string)
+	ingress, _ := logger.Get(ctx, "ingress").(string)
 
-	_backendConnections.inc(addr, serviceType, serviceName)
+	_backendConnections.inc(addr, namespace, serviceType, serviceName, ingress)
 
 	return &trackBackendConn{
 		Conn:        conn,
 		addr:        addr,
+		namespace:   namespace,
 		serviceType: serviceType,
 		serviceName: serviceName,
+		ingress:     ingress,
 	}
 }
 
@@ -103,14 +115,16 @@ type trackBackendConn struct {
 	net.Conn
 	addr        string
 	closed      int32
+	namespace   string
 	serviceType string
 	serviceName string
+	ingress     string
 }
 
 func (conn *trackBackendConn) Read(b []byte) (n int, err error) {
 	n, err = conn.Conn.Read(b)
 	if n > 0 {
-		_backendConnections.read(conn.addr, conn.serviceType, conn.serviceName, n)
+		_backendConnections.read(conn.addr, conn.namespace, conn.serviceType, conn.serviceName, conn.ingress, n)
 	}
 	return
 }
@@ -118,14 +132,14 @@ func (conn *trackBackendConn) Read(b []byte) (n int, err error) {
 func (conn *trackBackendConn) Write(b []byte) (n int, err error) {
 	n, err = conn.Conn.Write(b)
 	if n > 0 {
-		_backendConnections.write(conn.addr, conn.serviceType, conn.serviceName, n)
+		_backendConnections.write(conn.addr, conn.namespace, conn.serviceType, conn.serviceName, conn.ingress, n)
 	}
 	return
 }
 
 func (conn *trackBackendConn) Close() error {
 	if atomic.CompareAndSwapInt32(&conn.closed, 0, 1) {
-		_backendConnections.dec(conn.addr, conn.serviceType, conn.serviceName)
+		_backendConnections.dec(conn.addr, conn.namespace, conn.serviceType, conn.serviceName, conn.ingress)
 	}
 	return conn.Conn.Close()
 }
