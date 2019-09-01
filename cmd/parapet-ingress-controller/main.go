a14359f363241884466d8be3cfa8db7e16b59fd5
Thanatat Tamtan <acoshift@gmail.com>
2019-09-01 08:44:45 +0700

    Revert "main: change tls min version to tls1.0"
    This reverts commit 0c291f0816673d0ce55ce3a50ca7f3bfaae4196f.

--- cmd/parapet-ingress-controller/main.go
+++ cmd/parapet-ingress-controller/main.go
@@ -119,7 +119,6 @@ func main() {
 			WaitBeforeShutdown: 15 * time.Second,
 			Handler:            http.NotFoundHandler(),
 			TLSConfig: &tls.Config{
-				MinVersion:               tls.VersionTLS10,
 				PreferServerCipherSuites: true,
 				Certificates:             []tls.Certificate{cert},
 				GetCertificate:           ctrl.GetCertificate,
