a68fb8e4baebb3583e085f6357d22a8d303a1314
Thanatat Tamtan <acoshift@gmail.com>
2019-09-01 08:44:51 +0700

    Revert "main: use go default tls config"
    This reverts commit 0519b320d2e35b0a0b14bac710f115a0cb7f00f1.

--- cmd/parapet-ingress-controller/main.go
+++ cmd/parapet-ingress-controller/main.go
@@ -119,9 +119,25 @@ func main() {
 			WaitBeforeShutdown: 15 * time.Second,
 			Handler:            http.NotFoundHandler(),
 			TLSConfig: &tls.Config{
+				MinVersion: tls.VersionTLS12,
+				CurvePreferences: []tls.CurveID{
+					tls.X25519,
+					tls.CurveP256,
+				},
 				PreferServerCipherSuites: true,
-				Certificates:             []tls.Certificate{cert},
-				GetCertificate:           ctrl.GetCertificate,
+				CipherSuites: []uint16{
+					tls.TLS_AES_256_GCM_SHA384,
+					tls.TLS_CHACHA20_POLY1305_SHA256,
+					tls.TLS_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				},
+				Certificates:   []tls.Certificate{cert},
+				GetCertificate: ctrl.GetCertificate,
 			},
 		}
 		tlsSessionTicketKey := config.Base64("TLS_SESSION_TICKET_KEY")
