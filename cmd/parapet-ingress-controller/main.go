4b3e05c45a90bf7b7100e9398421686dc1047d64
Thanatat Tamtan <acoshift@gmail.com>
2019-09-01 08:51:35 +0700

    main: use default cupher suites

--- cmd/parapet-ingress-controller/main.go
+++ cmd/parapet-ingress-controller/main.go
@@ -125,19 +125,8 @@ func main() {
 					tls.CurveP256,
 				},
 				PreferServerCipherSuites: true,
-				CipherSuites: []uint16{
-					tls.TLS_AES_256_GCM_SHA384,
-					tls.TLS_CHACHA20_POLY1305_SHA256,
-					tls.TLS_AES_128_GCM_SHA256,
-					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				},
-				Certificates:   []tls.Certificate{cert},
-				GetCertificate: ctrl.GetCertificate,
+				Certificates:             []tls.Certificate{cert},
+				GetCertificate:           ctrl.GetCertificate,
 			},
 		}
 		tlsSessionTicketKey := config.Base64("TLS_SESSION_TICKET_KEY")
