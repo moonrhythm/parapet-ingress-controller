b8f88143ab8215efcce6d361d186d08053b94f5a
Thanatat Tamtan <acoshift@gmail.com>
2020-10-10 12:52:21 +0700

    metric: add reload counter (#4)

---
+++ metric/reload.go
@@ -0,0 +1,35 @@
+package metric
+
+import (
+	"github.com/moonrhythm/parapet/pkg/prom"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var _reload reload
+
+type reload struct {
+	vec *prometheus.CounterVec
+}
+
+func init() {
+	_reload.vec = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: prom.Namespace,
+		Name:      "reload",
+	}, []string{"success"})
+	prom.Registry().MustRegister(_reload.vec)
+}
+
+func Reload(success bool) {
+	l := prometheus.Labels{
+		"success": "0",
+	}
+	if success {
+		l["success"] = "1"
+	}
+
+	counter, err := _reload.vec.GetMetricWith(l)
+	if err != nil {
+		return
+	}
+	counter.Inc()
+}
