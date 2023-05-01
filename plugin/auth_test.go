e8843361cc11b7043ce2441caf6d9603f2929068
Thanatat Tamtan <acoshift@gmail.com>
2023-05-01 15:58:43 +0700

    plugin: forward auth test

--- plugin/auth_test.go
+++ plugin/auth_test.go
@@ -1,6 +1,7 @@
 package plugin_test
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -43,6 +44,70 @@ func TestBasicAuth(t *testing.T) {
 		var called bool
 		r := httptest.NewRequest(http.MethodGet, "/", nil)
 		r.SetBasicAuth("admin", "super")
+		w := httptest.NewRecorder()
+		ctx.ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			called = true
+		})).ServeHTTP(w, r)
+		assert.False(t, called)
+	})
+
+	t.Run("Empty", func(t *testing.T) {
+		var called bool
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		w := httptest.NewRecorder()
+		ctx.ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			called = true
+		})).ServeHTTP(w, r)
+		assert.False(t, called)
+	})
+}
+
+func TestForwardAuth(t *testing.T) {
+	t.Parallel()
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "Bearer super-secret-token" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		w.WriteHeader(http.StatusUnauthorized)
+	}))
+	defer ts.Close()
+
+	config := fmt.Sprintf(`
+url: %s
+authRequestHeaders:
+- authorization
+`, ts.URL)
+
+	ctx := Context{
+		Middlewares: &parapet.Middlewares{},
+		Ingress: &networking.Ingress{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					"parapet.moonrhythm.io/forward-auth": config,
+				},
+			},
+		},
+	}
+	ForwardAuth(ctx)
+
+	t.Run("Valid", func(t *testing.T) {
+		var called bool
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.Header.Set("Authorization", "Bearer super-secret-token")
+		w := httptest.NewRecorder()
+		ctx.ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			called = true
+		})).ServeHTTP(w, r)
+		assert.True(t, called)
+	})
+
+	t.Run("Invalid", func(t *testing.T) {
+		var called bool
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.Header.Set("Authorization", "Bearer wrong-token")
 		w := httptest.NewRecorder()
 		ctx.ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			called = true
