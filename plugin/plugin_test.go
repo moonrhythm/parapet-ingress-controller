4febee5e4e188187ebeaa84cfe923948a008a2eb
Thanatat Tamtan <acoshift@gmail.com>
2023-05-02 22:52:18 +0700

    plugin: inject state ingress test

---
+++ plugin/plugin_test.go
@@ -0,0 +1,42 @@
+package plugin_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/moonrhythm/parapet"
+	"github.com/stretchr/testify/assert"
+	networking "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	. "github.com/moonrhythm/parapet-ingress-controller/plugin"
+	"github.com/moonrhythm/parapet-ingress-controller/state"
+)
+
+func TestInjectStateIngress(t *testing.T) {
+	t.Parallel()
+
+	ctx := Context{
+		Middlewares: &parapet.Middlewares{},
+		Ingress: &networking.Ingress{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "default",
+				Name:      "ingress",
+			},
+		},
+	}
+	ctx.Use(state.Middleware())
+	InjectStateIngress(ctx)
+
+	var called bool
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+	ctx.ServeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		s := state.Get(r.Context())
+		assert.Equal(t, "default", s["namespace"])
+		assert.Equal(t, "ingress", s["ingress"])
+		called = true
+	})).ServeHTTP(w, r)
+	assert.True(t, called)
+}
